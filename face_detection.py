69c3a169155ba7c4b46a409688fc979bb3698203
Parag Sharma <sharmaparag2004@gmail.com>
2024-03-10 13:03:02 +0530

    update

--- face_detection.py
+++ face_detection.py
@@ -10,4 +10,9 @@ haar_cascade = cv.CascadeClassifier('haar_face.xml')
 face_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
 print(f'No. of faces detected = {len(face_rect)}')
 
+for (x, y, w, h) in face_rect:
+    cv.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+
+cv.imshow('detected faces', img)
+
 cv.waitKey(0)
