42609e5659ee9c4fdce665d87559c6ad4ef22d6a
Parag Sharma <sharmaparag2004@gmail.com>
2024-03-18 23:13:31 +0530

    update

--- face_train.py
+++ face_train.py
@@ -5,6 +5,9 @@ import numpy as np
 things = ['apple', 'book', 'chiar']
 DIR = r'C:\Users\Administrator\Downloads\train'
 
+
+haar_cascade = cv.CascadeClassifier('haar_face.xml')
+
 features = []
 labels = []
 def create_train():
@@ -14,3 +17,19 @@ def create_train():
 
         for img in os.listdir(path):
             img_path = os.path.join(path, img)
+
+
+            img_array = cv.imread(img_path)
+            grey = cv.cvtColor(img_array, cv.COLOR_BGR2GRAY)
+
+            faces_rect = haar_cascade.detectMultiScale(grey, scaleFactor=1.1, minNeighbors = 4 )
+
+            for (x, y, w, h) in faces_rect:
+                faces_roi = grey[y:y+h, x:x+w]
+                features.append(faces_roi)
+                labels.append(label)
+create_train()
+
+print(f'Length of the features = {len(features)}')
+print(f'Length of the labels = {len(labels)}')
+
